b97d867eea626c6f7e4a33ad15f655eff09ac8bc
VedangiPatel10 <vedangipatel1005@gmail.com>
2026-07-26 23:08:08 -0400

    Made a const addToDo

--- src/App.Solution.jsx
+++ src/App.Solution.jsx
@@ -5,5 +5,26 @@ function App() {
     const [task, setTask] = useState("");
     const [todos, setTodos] = useState([]);
 
-    
+    const addToDo = () => {
+        if (task.trim() !== "") const newTodo = {
+      id: Date.now(),
+      text: task,
+      completed: false
+    };
+ 
+    setTodos([
+      ...todos,
+      newTodo
+    ]);
+ 
+    setTask("");
+  };
+   const deleteTodo = (id) => {
+    setTodos(
+      todos.filter(
+        (item) => item.id !== id
+      )
+    );
+};
+
 }
